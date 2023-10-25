eb6d08cbff42fb23fd6a6916fea269b4d46f3aaf
jos-gabriel <jos.gabrie@gmail.com>
2023-10-24 18:21:49 -0600

    firstCommit: Demo SpringBoot

---
+++ spring-demo/src/test/java/com/gabriel/minutes/springdemo/SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.minutes.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
